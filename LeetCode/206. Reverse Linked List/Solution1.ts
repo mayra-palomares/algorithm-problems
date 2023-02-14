b9e4b693aabe0f11434a4af19fb7e82260b60e3d
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-02-14 21:12:32 +0000

    LeetCode: Reverse Linked List

---
+++ LeetCode/206. Reverse Linked List/Solution1.ts
@@ -0,0 +1,23 @@
+/* Definition for singly-linked list. */
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+ 
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let reversedList: ListNode | null = null
+    let currentNode: ListNode | null = head
+    
+    while(currentNode !== null){
+        const nodeToInsert: ListNode = new ListNode(currentNode.val, reversedList)
+        reversedList = nodeToInsert
+        currentNode = currentNode.next
+    }
+    
+    return reversedList
+};
