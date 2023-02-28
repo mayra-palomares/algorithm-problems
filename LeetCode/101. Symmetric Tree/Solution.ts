3634e2544576bdb47b30883b49b747f92c64d5b1
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-02-28 22:12:21 +0000

    LeetCode: Path Sum II

--- LeetCode/101. Symmetric Tree/Solution.ts
+++ LeetCode/101. Symmetric Tree/Solution.ts
@@ -9,7 +9,7 @@ class TreeNode {
         this.left = (left===undefined ? null : left)
         this.right = (right===undefined ? null : right)
     }
- }
+}
  
 
 function isSymmetric(root: TreeNode | null): boolean {
